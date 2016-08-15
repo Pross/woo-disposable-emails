f4f32ebf5b63f2d89dbaaa33266d3b099ec4fcec
Simon Prosser <pross@pross.org.uk>
2016-08-15 22:45:30 +0100

    init

---
+++ includes/class-disposable-emails.php
@@ -0,0 +1,78 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Disposable Email Class
+ *
+ * @package  WC_Integration_Disposable_Emails
+ * @category Integration
+ * @author   Simon Prosser
+ */
+
+if ( ! class_exists( 'WC_Integration_Disposable_Emails' ) ) :
+
+class WC_Integration_Disposable_Emails extends WC_Integration {
+
+ /**
+  * Init and hook in the integration.
+  */
+ public function __construct() {
+   global $woocommerce;
+
+   $this->id                 = 'integration-disposable-emails';
+   $this->method_title       = __( 'Disposable Emails', 'integration-disposable-emails' );
+   $this->method_description = __( 'Filter out unwanted users of disposable emails.', 'integration-disposable-emails' );
+
+   // Load the settings.
+   $this->init_form_fields();
+
+   // Actions.
+   add_action( 'woocommerce_update_options_integration_' .  $this->id, array( $this, 'process_admin_options' ) );
+
+  // add_filter( 'is_email', array( $this, 'is_email' ) );
+
+   add_filter( 'registration_errors', array( $this, 'registration_errors' ), 100, 3 );
+ }
+
+ function registration_errors( $errors, $sanitized_user_login, $user_email ) {
+
+   if( empty( $errors->errors ) && 'yes' === $this->get_option( 'enabled' ) ) {
+     if( $this->is_temp_mail( $user_email ) ) {
+       $errors->add( 'invalid_email', $this->get_option( 'error' ) );
+     }
+   }
+   return $errors;
+ }
+
+ function is_temp_mail($mail) {
+
+    $mail_domains_ko = file( WC_Disposable_Emails::path() . 'vendor/disposable-email-domains/disposable_email_blacklist.conf', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    //Need to ensure the mail contains an @ to avoid undefined offset
+    return in_array(explode('@', $mail)[1], $mail_domains_ko);
+ }
+
+ /**
+  * Initialize integration settings form fields.
+  */
+ public function init_form_fields() {
+   $this->form_fields = array(
+     'enabled' => array(
+       'title'             => __( 'Enable Disposable Email Filter', 'integration-disposable-emails' ),
+       'type'              => 'checkbox',
+       'description'       => __( 'All signups will be checked against the email list', 'integration-disposable-emails' ),
+       'desc_tip'          => true,
+       'default'           => 'no'
+     ),
+     'error' => array(
+       'title'             => __( 'Error Message', 'integration-disposable-emails' ),
+       'type'              => 'text',
+       'default'           => __( "<strong>Error: </strong>Sorry no disposable emails allowed.", "integration-disposable-emails" )
+     )
+   );
+ }
+
+}
+
+endif;
---
+++ woocommerce-disposable-emails.php
@@ -0,0 +1,44 @@
+<?php
+/**
+Plugin Name: Woocommerce Disposable Emails
+Description: Block disposable email addresses at signup.
+Author: Simon Prosser
+Author URI: https://pross.org.uk
+Version: 1.0
+*/
+
+if ( ! class_exists( 'WC_Disposable_Emails' ) ) :
+class WC_Disposable_Emails {
+ /**
+ * Construct the plugin.
+ */
+ public function __construct() {
+   add_action( 'plugins_loaded', array( $this, 'init' ) );
+ }
+ /**
+ * Initialize the plugin.
+ */
+ public function init() {
+   // Checks if WooCommerce is installed.
+   if ( class_exists( 'WC_Integration' ) ) {
+     // Include our integration class.
+     include_once 'includes/class-disposable-emails.php';
+     // Register the integration.
+     add_filter( 'woocommerce_integrations', array( $this, 'add_integration' ) );
+   } else {
+     // throw an admin error if you like
+   }
+ }
+ /**
+  * Add a new integration to WooCommerce.
+  */
+ public function add_integration( $integrations ) {
+   $integrations[] = 'WC_Integration_Disposable_Emails';
+   return $integrations;
+ }
+ public static function path() {
+   return plugin_dir_path( __FILE__ );
+ }
+}
+$WC_Disposable_Emails = new WC_Disposable_Emails( __FILE__ );
+endif;
